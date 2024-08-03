4f5e229e3c0360d9a14e22e2524ba3f64202eec8
smtnhacker <themikosurara@gmail.com>
2024-08-03 23:26:39 +0800

    style: functions page

--- client/src/routes/functions/+page.svelte
+++ client/src/routes/functions/+page.svelte
@@ -38,24 +38,38 @@ def swap(a, b):
     ]
 </script>
 
-<div>
-    <div>Functions</div>
-    <p>Often, there are set of instructions that you'll reuse at different parts of the program. Instead of manually copy-pasting these code and possibly make your code harder to debug and maintain, you can use Python's functions. They have the following format:</p>
-    <div>{@html marked("```python\ndef function_name(parameter1: type, parameter2: type) -> return_type:\n\t# function logic\n\treturn value")}</div>
-    <div>Here are some examples:</div>
-    <div>
-        {#each examples as example}
-            {@html marked(`\`\`\`python${example}`)}
-        {/each}
+<div class="flex justify-center content-stretch">
+    <div class="flex bg-white rounded-2xl w-7/12 text-black">
+        <div></div>
+        <div class="flex flex-col items-center grow">
+            <h2 class="text-5xl my-4 voyage-bold text-[var(--red-color)]">Functions</h2>
+            <div class="border-solid border-8 border-black montserrat-ital mx-14 mb-14 p-4 rounded-lg">
+                <p>Often, there are set of instructions that you'll reuse at different parts of the program. Instead of manually copy-pasting these code and possibly make your code harder to debug and maintain, you can use Python's functions. They have the following format:</p>
+                <div>{@html marked("```python\ndef function_name(parameter1: type, parameter2: type) -> return_type:\n\t# function logic\n\treturn value")}</div>
+                <div>Here are some examples:</div>
+                <div>
+                    {#each examples as example}
+                        {@html marked(`\`\`\`python${example}`)}
+                    {/each}
+                </div>
+                <h1 class="font-semibold text-xl">Test Yourself</h1>
+                <p>For this activity, since we have not yet covered other parts of Python, you'll just be tasked with defining a function that returns a proper question. It should follow the following template:</p>
+                <div>{@html marked("```python\ndef ask_question():\n\t# function logic\n\treturn \"Some question?\"")}</div>
+                <div class="mt-4">
+                    <form on:submit|preventDefault={uploadFile}>
+                        <input type="file" bind:this={fileInput} on:change={() => file = fileInput.files[0]} />
+                        <button type="submit" class="bg-slate-200 px-3 py-1 rounded-lg">Upload</button>
+                    </form>
+                    {#if output}
+                    <p>ChatGPT:</p>
+                    <div>{output}</div>
+                    {/if}
+                </div>
+            </div>
+        </div>
+        <div></div>
     </div>
-    <p>For this activity, since we have not yet covered other parts of Python, you'll just be tasked with defining a function that returns a proper question. It should follow the following template:</p>
-    <div>{@html marked("```python\ndef ask_question():\n\t# function logic\n\treturn \"Some question?\"")}</div>
 </div>
-<form on:submit|preventDefault={uploadFile}>
-    <input type="file" bind:this={fileInput} on:change={() => file = fileInput.files[0]} />
-    <button type="submit">Upload</button>
-    <p>Result: {output}</p>
-</form>
 
 <style>
     form {
