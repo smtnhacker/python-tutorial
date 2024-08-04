74bccb70056bf34d32ccc6e99e5a841e16803039
smtnhacker <themikosurara@gmail.com>
2024-08-05 02:09:15 +0800

    feat: add more mandala testcases

--- server/cases/mandala.py
+++ server/cases/mandala.py
@@ -1,3 +1,5 @@
+import random
+
 def gen_mandala(num_rows, num_cols, pattern):
     new_pattern = []
     for row in range(num_rows):
@@ -8,9 +10,31 @@ def gen_mandala(num_rows, num_cols, pattern):
         new_pattern.append(cur_row)
     return new_pattern
 
+def gen_testcase():
+    num_rows = random.randint(2,5)
+    num_cols = random.randint(1,10)
+    pattern = []
+    for _ in range(num_rows):
+        pattern.append("".join(random.choices("01", k=num_cols)))
+    return pattern
+
 testcases = [
    [
        "00",
        "01"
+   ],
+   [
+       "0001",
+       "0011",
+       "0111",
+       "1111"
+   ],
+   [
+       "01",
+       "10"
+   ],
+   [
+       
    ]
 ]
+testcases += [gen_testcase() for _ in range(20)]
